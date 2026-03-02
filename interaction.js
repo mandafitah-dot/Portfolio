17c2e125e4ed21c2a7dfd6a3b7e5c57aad02e1e1
Manda Fitahiana <mandafitah@gmail.com>
2026-03-02 22:57:58 +0100

    Ajout CV pdf

--- interaction.js
+++ interaction.js
@@ -10,3 +10,19 @@ document.querySelectorAll("section").forEach(section => {
     section.classList.add('hidden');
     observer.observe(section);  
 });
+
+const modal = document.getElementById("cvModal");
+const btn = document.getElementById("openCV");
+const closebtn = ddocument.querySelector(".close");
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+closebtn.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
